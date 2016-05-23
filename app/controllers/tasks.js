1d5aa66c5cd8dba03d9b476ad692d98bb57ee1ed
Mukesh Singh <msingh@ETMUM-MB013.local>
2016-05-23 16:07:33 +0530

    Fixed date issue.

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -6,6 +6,7 @@ _und = require("underscore");
 multer = require('multer');
 findRemoveSync = require('find-remove');
 XLSX = require('xlsx');
+var moment = require('moment-timezone');
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -192,16 +193,17 @@ router.post('/upload', uploading.single('file'), isLoggedIn, function(req, res) 
         task.user_id = req.user._id;
         task.user_name = req.user.name;
         task.verifier_id = null;
-        task.created_at = new Date(d).toLocaleDateString();
+        task.created_at = moment.tz(d, "Asia/Kolkata");
+        task.updated_at = moment.tz(d, "Asia/Kolkata");
         //  task.updated_at=new Date(req.body.date).toLocaleDateString();
         task.save(function(err) {
             if (err)
                 throw err;
         });
     });
-    // findRemoveSync(process.cwd() + '/tmp/', {
-    //     extensions: ['.xlsx']
-    // });
+    findRemoveSync(process.cwd() + '/tmp/', {
+        extensions: ['.xlsx']
+    });
     res.send({
         status: 'success',
         tasks_count: tasks_count

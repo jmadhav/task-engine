1698cf487a9d0f938909a308a877b77ca6447e44
Mukesh <mukesh23.singh@gmail.com>
2016-05-26 16:22:30 +0530

    Removed the extra js file

--- public/js/custom.js
+++ public/js/custom.js
@@ -671,4 +671,20 @@ $(document).ready(function(){
                     }
                 });
     }
+
+    function setInitialDate() {
+        var path = window.location.pathname;
+        path = path.replace(/\/$/, "");
+        path = decodeURIComponent(path);
+        if (path == '/view_task') {
+          $('#viewTaskFromDate').val($.format.date(new Date, "MM/dd/yyyy"));
+          $('#viewTaskToDate').val($.format.date(new Date, "MM/dd/yyyy"));
+          $('#viewTaskSubmit').trigger('click');
+        }
+        if (path == '/upload') {
+          $('#uploadDate').val($.format.date(new Date, "MM/dd/yyyy"));
+        }
+
+    }
+    setInitialDate();
 });
